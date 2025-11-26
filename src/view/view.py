6404f4fba48f90fbc0052a3fc18db9546f3ef891
Kimviruscool <sinsa9122@naver.com>
2025-11-26 15:10:41 +0900

    page

--- src/view/view.py
+++ src/view/view.py
@@ -5,3 +5,27 @@ bp = Blueprint('main', __name__, url_prefix='/')
 @bp.route('/')
 def home():
     return render_template('home.html')
+
+@bp.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
+
+@bp.route('/stocks')
+def stocks():
+    return render_template('stocks.html')
+
+@bp.route('/coins')
+def coins():
+    return render_template('coins.html')
+
+@bp.route('/guestbook')
+def guestbook():
+    return render_template('guestbook.html')
+
+@bp.route('/commits')
+def commits():
+    return render_template('commits.html')
+
+@bp.route('/record')
+def record():
+    return render_template('record.html')
